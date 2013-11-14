7531681d1f38d7226542b2ead74ad95326a6cb32
Francois Lebel <francoislebel@gmail.com>
2013-11-14 03:46:03 -0500

    Moved the license out of the script.

--- cisco79xx_phone_directory.py
+++ cisco79xx_phone_directory.py
@@ -1,23 +1,5 @@
 #!/usr/bin/env python
 
-#   Cisco 79xx phone directory: a Flask app to use Google Contacts
-#   as the phone directory for the Cisco 79xx IP phones.
-#   Copyright (C) 2010-2011 Francois Lebel <francoislebel@gmail.com>
-#   http://github.com/flebel/cisco79xx_phone_directory
-#
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
-#
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
-#
-#   You should have received a copy of the GNU General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 import re
 import unicodedata
 from flask import Flask, request
